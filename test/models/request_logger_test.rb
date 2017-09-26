314ba8ddb207af5fb0b61634944b5c0e0a1efa14
Carl-Henrik <carl-henrik.perssonn@gmail.com>
2017-09-26 11:05:44 +0200

    fixed some bad code in request_logger_test.

--- test/models/request_logger_test.rb
+++ test/models/request_logger_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class RequestLoggerTest < ActiveSupport::TestCase
-  # Sets up a controller request objects env hash with relevant accepted data.
+  # Sets up a hash with relevant accepted data simulating the env hash of a request
   def setup
-    controller.request.env.assign_attributes({
+      @env = {
       REMOTE_HOST: "test",
       REMOTE_ADDR: "127.0.0.1",
       HTTP_USER_AGENT: "chrome",
       HTTP_REFERER: "www.bredband2.com",
-      REQUEST_URI: "/test"]
-      })
+      REQUEST_URI: "/test"
+      }
   end
 
-  # Send env hash to logg_request method in RequestLogger
+  # Send hash to logg_request method in RequestLogger
   # Checks number of visits stored in db to verify that the record was saved.
   test "should save correct request to visits" do
     assert_difference('Visit.count') do
-      RequestLogger.logg_request(controller.request.env)
+      RequestLogger.logg_request(@env)
     end
   end
 
@@ -25,9 +25,9 @@ class RequestLoggerTest < ActiveSupport::TestCase
   # Checks number of visits stored in db to verify that the nil values were replaced and record was saved.
   test "should convert nil attributes before save" do
     assert_difference('Visit.count') do
-      controller.request.env["REMOTE_HOST"] = nil
-      controller.request.env["REQUEST_URI"] = nil
-      RequestLogger.logg_request(controller.request.env)
+      @env["REMOTE_HOST"] = nil
+      @env["REQUEST_URI"] = nil
+      RequestLogger.logg_request(@env)
     end
   end
 
