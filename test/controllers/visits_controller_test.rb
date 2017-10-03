79f49d064e97ef212179599d4e636dd3b2fe7cf9
Carl-Henrik <carl-henrik.perssonn@gmail.com>
2017-10-03 15:12:38 +0200

    fixed som broken tests. removed som bad lines from docker files.

--- test/controllers/visits_controller_test.rb
+++ test/controllers/visits_controller_test.rb
@@ -7,14 +7,20 @@ class VisitsControllerTest < ActionDispatch::IntegrationTest
     @admin = admins(:one)
   end
 
+  # signs in since controller is restricted
+  # asserts response
   test "should get index" do
     sign_in(@admin)
     get visits_url
-    assert_redirected_to visits_path
+    assert_response :success
   end
-
+  # signs in since controller is restricted
+  # calls empty_logg in visits controller
+  # confirms that the visits table is empty
   test "should empty visits table" do
     sign_in(@admin)
+    get empty_logg_visits_path
+    assert_equal [], Visit.all
   end
 
 end
